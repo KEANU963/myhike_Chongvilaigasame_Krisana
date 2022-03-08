2c46633a44d58456500485f0d94c14b96b758280
Krisana C <ksnch777@gmail.com>
2022-03-07 20:40:29 -0800

    editted main.html and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,32 +3,40 @@ function read_display_quote() {
 
     // get into the right collection
     db.collection('quotes').doc('tuesday')                                              //name of the collection and documents should matach excatly with what you have in Firestore
-    .onSnapshot(tuesdayDoc => {                                                         //arrow notation
-        console.log("current document data: " + tuesdayDoc.data());                     //.data() returns data object
-        document.getElementById('quote-goes-here').innerHTML=tuesdayDoc.data().quote;   //using javascript to display the data on the right place 
-    })
+        .onSnapshot(tuesdayDoc => {                                                         //arrow notation
+            console.log("current document data: " + tuesdayDoc.data());                     //.data() returns data object
+            document.getElementById('quote-goes-here').innerHTML = tuesdayDoc.data().quote;   //using javascript to display the data on the right place 
+        })
 }
 
 read_display_quote();       //calling the funciton
 
-function insertName(){
-   // to check if the user is logged in:
-   firebaseConfig.auth().onAuthStateChanged(user => {
-    if (user){
-        console.log(user.uid); //let me to know who is the user that logged in to get the UID
-        currentUser = db.collection('users').doc(user.uid); // will go to the frstore and got to the doc of the user
-        currentUser.get().then(userDoc => {
-            // get the user name
-            var userName = userDoc.data().name;
-            console.log(userName);
-            document.getElementById("name-goes-here").innerText=userName;
-            $('#name-goes-here'.text(userName)); //jQuery
-            // document.getElementByID()
-        })
-    }
 
-    
-   })
+function insertName() {
+
+    // give me access to login uid
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(
+        /*a way to write a function*/ // function(user){}
+        /*another way to write a function*/ 
+        user => {
+            if (user){
+                console.log(user.uid); // let me know who is the user that logged in to get the uid
+                let currentUser = db.collection('users').doc(user.uid); //will go to the firestore and go to the document of the user
+                currentUser.get().then(userDoc=>{
+                    // get username
+                    let userName = userDoc.data().name;
+                    console.log(userName);
+                    $('#name-goes-here').text(userName); //jQuery way
+                    // document.getElementById('#name-goes-here').innerText=userName; ????????
+                })
+            
+            }
+
+
+        }
+    );
+
 }
 
-insertName();
+insertName();  //calling the function
